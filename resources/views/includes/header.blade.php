7e8ccfd3975534d1cd383ea42d8fa513caaa9554
karlerki1 <karlerki@ut.ee>
2016-05-31 14:04:28 +0300

    esmanestruktuur

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="#">vaata kuulutusi</a></li>
+      <li><a href="#">lisa kuulutus</a></li>
+      <li><a href="#">meist</a></li>
+
+    </ul>
+  </nav>
+</header>
